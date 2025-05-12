614c9ab0323b9420a0e602ccca41f9b23e3c6bab
IndraDevOpsX <huapayadavid143@gmail.com>
2025-05-12 11:53:03 -0500

    pruebas

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
     return Controller.extend("prueba.controller.Home", {
         onInit() {
-            //Prueba CI/CD III
+            //Prueba CI/CD Iv
         }
     });
 });
